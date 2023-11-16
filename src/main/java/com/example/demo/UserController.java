38811dc1b9db49be8fc4bd478009a35d30ce76ca
Your GitHub Username <Your GitHub Email Address>
2023-11-16 19:55:29 +0200

    test

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo() {
-        return "How?!";
+        return "How?!13255252";
     }
 }
